738b4e7e20e3c9957272ecea65eb5a0ee4404e72
RamanSlinka <romuch1986@gmail.com>
2022-06-15 22:28:58 +0200

    created base server, added express, nodemon, MongoDB + mongoose

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 import express from 'express';
+import mongoose from "mongoose";
 
 const PORT = 5000;
-
+const DB_URL = 'mongodb+srv://user:user@cluster0.jz2fye9.mongodb.net/?retryWrites=true&w=majority'
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`server started on PORT ${PORT}`)
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.status(200).json('server work')
 })
+
+async function startApp() {
+    try {
+        await mongoose.connect(DB_URL)
+        app.listen(PORT, () => {
+            console.log(`server started on PORT ${PORT}`)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+startApp()
