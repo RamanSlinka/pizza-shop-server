0e310f5511f923887cfe17849a869049d5907b6a
RamanSlinka <romuch1986@gmail.com>
2022-07-03 19:23:54 +0200

    refactored code and changed "check" func; added error handler (wrapped into "try/catch")

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,46 +15,83 @@ const generateJwt = (id, email, role) => {
 
 class UserController {
     async registration(req, res, next) {
-        const {email, password, role} = req.body
-        if (!email || !password) {
-            return next(ApiError.bedRequest('Incorrect email or password'))
-        }
-        const candidate = await User.findOne({email})
-        if (candidate) {
-            return next(ApiError.bedRequest('User already exist'))
-        }
-        const hashPassword = await bcrypt.hash(password, 5)
-        const user = new User({
-            email: req.body.email,
-            role: req.body.role,
-            password: hashPassword
-        })
-        await user.save()
 
-        // const basket = await Basket.create({userId: user._id})
-        // use when will be "Basket" Schema
+        try {
+            const {email, password, role} = req.body
+            if (!email || !password) {
+                return next(ApiError.bedRequest('Incorrect email or password'))
+            }
+            const candidate = await User.findOne({email})
+            if (candidate) {
+                return next(ApiError.bedRequest('User already exist'))
+            }
+            const hashPassword = await bcrypt.hash(password, 5)
+            const user = new User({
+                email: req.body.email,
+                role: req.body.role,
+                password: hashPassword
+            })
+            await user.save()
 
-        const token = generateJwt(user._id, user.email, user.role)
-        return res.json({token})
+            // const basket = await Basket.create({userId: user._id})
+            // use when will be "Basket" Schema
+
+            const token = generateJwt(user._id, user.email, user.role)
+            return res.json({token})
+
+        } catch (e) {
+            console.log(e)
+            return res.status(500).json(e)
+        }
+
     }
 
-    async login(req, res, next) {
-        const {email, password} = req.body
-        const user = await User.findOne({email})
-        if (!user) {
-            return next(ApiError.internal('User not find'))
+    async login(req, res) {
+
+        try {
+            const {email, password} = req.body
+            const user = await User.findOne({email})
+            if (!user) {
+                return res.status(404).json({message: "User not found"})
+            }
+            let comparePassword = bcrypt.compareSync(password, user.password)
+            if (!comparePassword) {
+                return res.status(400).json({message: "Invalid password"})
+            }
+            const token = generateJwt(user._id, user.email, user.role)
+            return res.json({
+                token,
+                user: {
+                    id: user.id,
+                    email: user.email,
+                    role: user.role
+                }
+            })
+
+        } catch (e) {
+            console.log(e)
+            return res.status(500).json(e)
         }
-        let comparePassword = bcrypt.compareSync(password, user.password)
-        if (!comparePassword) {
-            return next(ApiError.internal('Password wrong'))
-        }
-        const token = generateJwt(user._id, user.email, user.role)
-        return res.json({token})
+
     }
 
-    async check(req, res, next) {
-        const token = generateJwt(req.user._id, req.user.email, req.user.role)
-        return res.json({token})
+    async check(req, res) {
+        try {
+            const user = await User.findOne({_id: req.user.id})
+            const token = generateJwt(req.user._id, req.user.email, req.user.role)
+            return res.json({
+                token,
+                user: {
+                    id: user.id,
+                    email: user.email,
+                    role: user.role
+                }
+            })
+        } catch (e) {
+            console.log(e)
+            return res.status(500).json(e)
+        }
+
     }
 }
 
